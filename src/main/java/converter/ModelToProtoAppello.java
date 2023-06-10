3aa19a0f8a2e9e5138ae7ee0bdf983cd4b187b7e
ale_9702 <alessandroruffolo29@gmail.com>
2023-06-10 19:34:07 +0200

    Modifica dataOra degli appelli in risposta

--- src/main/java/converter/ModelToProtoAppello.java
+++ src/main/java/converter/ModelToProtoAppello.java
@@ -4,6 +4,8 @@ import model.Appello;
 import model.Models;
 import proto.Remotemethod;
 
+import java.util.Calendar;
+
 public class ModelToProtoAppello implements Convertitore{
 
     public Remotemethod.Appello convert(Models model){
@@ -11,6 +13,9 @@ public class ModelToProtoAppello implements Convertitore{
             throw new ClassCastException("Tipo passato non supportato per AbstractFactory");
         }
         Appello appello = (Appello)model;
-        return Remotemethod.Appello.newBuilder().setId(appello.getId()).setDurata(appello.getDurata()).setOra(appello.getOra().toString()).setNome(appello.getNome()).build();
+        Calendar dataOra = appello.getOra();
+        String riassunto = ""+ dataOra.get(Calendar.DAY_OF_MONTH)+"-"+ dataOra.get(Calendar.MONTH) + "-" + dataOra.get(Calendar.YEAR)
+                + "    " + dataOra.get(Calendar.HOUR_OF_DAY) + ":" + dataOra.get(Calendar.MINUTE) + ":" + dataOra.get(Calendar.SECOND);
+        return Remotemethod.Appello.newBuilder().setId(appello.getId()).setDurata(appello.getDurata()).setOra(riassunto).setNome(appello.getNome()).build();
     }
 }
